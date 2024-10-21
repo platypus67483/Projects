f7da79a696c66f72a256b3d115aec686c4e226dc
smithbwlhs <smithb@wlwv.k12.or.us>
2024-10-21 13:59:51 -0700

    class code added

---
+++ Zookeeper/src/Zookeeper.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Zookeeper {
+    public static void main(String[] args) {
+        String animalType = "Camel";
+        String time = "4:37PM";
+        double habitatTemp = 59.66;
+
+        System.out.println("Animal type: " + animalType);
+        System.out.println("Current time: " + time);
+        System.out.println("Habitat temperature: " + habitatTemp+"F");
+
+        String camel = """
+            Look at our camel
+            
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;     
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+        
+                """;
+        System.out.println(camel);
+
+        File file = new File("Zookeeper/animals/hippo.txt");
+        try{
+            Scanner fr = new Scanner(file);
+            while(fr.hasNextLine()){
+                String i = fr.nextLine();
+                System.out.println(i);
+            }
+        } catch (Exception e){
+            System.out.println("No file");
+        }
+
+
+    }
+}
