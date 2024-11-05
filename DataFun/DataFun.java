511d07c23f2dfc55125ec521c51523024de48746
Brandon Smith <88860661+smithbwlhs@users.noreply.github.com>
2024-11-05 09:40:58 -0800

    Create DataFun.java

---
+++ DataFun/DataFun.java
@@ -0,0 +1,12 @@
+// any imports here
+
+/**
+     * Description goes here
+     * @author [Student Name]
+     * @version [Current Date]
+     */
+public class DataFun{
+    public static void main(String[] args){
+        
+    }
+}
