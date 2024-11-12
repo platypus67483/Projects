4b8857a64c3861ad675aa6117ce8ca62c157213c
6-4-1989 <kelin19772@gmail.com>
2024-11-12 15:04:24 -0800

    Commit Message

--- DataFun/DataFun.java
+++ DataFun/DataFun.java
@@ -1,12 +1,36 @@
-// any imports here
+import java.util.Scanner;
 
 /**
-     * Description goes here
-     * @author [Student Name]
-     * @version [Current Date]
+     Data Fun Project
+     Name: Kevin Lin
+     Date Due: November 15th, 2024
      */
-public class DataFun{
-    public static void main(String[] args){
+public class DataFun
+{
+    public int favoriteNumber;
+    public String favoriteKeyboardCharacter;
+    
+    public void IterateThroughStuffPartOne()
+    {
         
+    }
+    public void IterateThroughStuffPartTwo()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        DataFun noDataFun = new DataFun();
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("What's your favorite number lah>\n");
+        favoriteNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        noDataFun.IterateThroughStuffPartOne();
+        
+        System.out.println("What's your favorite keyboard character lah>\n");
+        favoriteKeyboardCharacter = scanner.nextLine();
+        noDataFun.IterateThroughStuffPartTwo();
     }
 }
